432d28ca9050762972298a58649124a82067234d
kopteffmax <kopteffmax@yandex.ru>
2017-10-10 19:45:27 +0300

    added blog data to the store because it'd be used in footer component

--- src/components/blog.vue
+++ src/components/blog.vue
@@ -29,27 +29,19 @@
 </template>
 
 <script>
-import imgGirl from '../assets/images/imgGirl.png';
-import imgLaptop from '../assets/images/imgLaptop.png';
-import imgTablet from '../assets/images/imgTablet.png';
-
 export default {
   data () {
     return {
       mainHeading: 'Latest blog',
-      blogs: [{heading: 'Lorem ipsum dolor sit amet', content: 'Consectetuer adipiscing elit. Aenean commodo ligula ' +
-                        'eget dolor. Aenean massa.', date: {month: 'Jan', day: '15'}, views: 542, comments: 17,
-                        img: imgGirl},
-              {heading: ' Donec pede justo, fringilla vel', content: 'Cum sociis natoque penatibus et magnis dis parturient ' +
-              ' montes, nascetur ridiculus mus. ', date: {month: 'Jan', day: '14'}, views: 992, comments: 42, img: imgLaptop},
-              {heading: 'Aenean vulputate eleifend tellus', content: 'Etiam sit amet orci eget eros faucibus tincidunt.',
-               date: {month: 'Jan', day: '12'}, views: 1560, comments: 98, img: imgTablet}],
       mapHeading: 'open map'
     }
   },
   computed: {
       headings() {
           return this.$store.state.headings;
+      },
+      blogs() {
+          return this.$store.state.blogs;
       }
   },
   filters: {
